fa419286f3daebc97aabdf1433b62fc145cc229d
audeangelo <audeangelo.aa@gmail.com>
2025-06-03 10:09:32 +0200

    page elagage redon

--- main.js
+++ main.js
@@ -166,3 +166,20 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
   
+
+
+
+/* ========== CARROUSSEL PAGE ELAGAGE ========== */
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const track = document.querySelector('.carousel-track');
+    const items = document.querySelectorAll('.carousel-item');
+    let index = 0;
+
+    function moveCarousel() {
+      index = (index + 1) % items.length;
+      track.style.transform = `translateX(-${index * 100}%)`;
+    }
+
+    setInterval(moveCarousel, 2500); // 2.5s = 2s pause + 0.5s transition
+  });
